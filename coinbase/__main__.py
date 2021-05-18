543c7bb828d391dc0331236795d948d698815b5d
Andrew Palmer <atp@sdf.org>
2021-05-18 16:37:23 -0500

    Extract http module

--- coinbase/__main__.py
+++ coinbase/__main__.py
@@ -1,40 +1,6 @@
-import hashlib
-import hmac
-import os
-import time
-from pprint import pprint
-
 import click
-import dotenv
-import requests
-
-
-dotenv.load_dotenv()
-
-
-def access_sign(ts, method, path, body):
-    secret = os.getenv('API_SECRET')
-    prehash = ''.join((str(ts), method, path, body))
-    return hmac.new(
-        secret.encode('utf8'),
-        prehash.encode('utf8'),
-        hashlib.sha256
-    ).hexdigest()
-
-
-def request(method, path):
-    ts = int(time.time())
-    response = requests.request(
-        method,
-        f'https://api.coinbase.com{path}',
-        headers={
-            'CB-ACCESS-KEY': os.getenv('API_KEY'),
-            'CB-ACCESS-SIGN': access_sign(ts, method, path, ''),
-            'CB-ACCESS-TIMESTAMP': str(ts),
-            'CB-VERSION': '2017-11-26',
-        },
-    )
-    return response.json()
+from pprint import pprint
+from .http import request
 
 
 @click.group()
